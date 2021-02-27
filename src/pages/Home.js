24faebc1f950011371ddaede3ef72d849198b9f8
alhoffiq <andrew.hoff14@yahoo.com>
2021-02-26 22:52:20 -0700

    Added badge info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import Me from "../assets/images/Selfie.jpg";
 import Image from "react-bootstrap/Image";
 import Skills from "../components/Skills";
+import badge from "../assets/badge.json";
 
 function Home() {
     return (
@@ -17,6 +18,7 @@ function Home() {
                     </div>
                     <div className="col-md-8">
                         <p className="big">Hello! I'm Andrew Hoff and I'm currently a pharmacy technician for Walgreens but I am eager to start my web develoment career. I've gained my current skills from graduating University of Arizona's Web Development Bootcamp and I'm always learning more on my own.</p>
+                        <a href={badge.id} target="_blank" rel="noreferrer"><img className="badge rounded" src={badge.image.id} alt="badge"></img></a>
                         <br></br>
                         <p className="big">I grew up in front of a computer screen and I always knew I'd have a career making them work. I'm always wondering how things work and I enjoy breaking things down into their parts to figure out every one of their roles and how the accomplish them. I'm constantly looking at my computer wondering just how it's doing what it's doing and even with my knowledge of them I'm convinced it's just magic.</p>
                         <br></br>
